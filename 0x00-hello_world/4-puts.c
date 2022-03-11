53cf41d499e97f4a4377b8b27413ae66248dfd66
mohammedmussa <mohammedismael902@gmail.com>
2022-03-11 23:36:16 +0300

    lc4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include "studio.h"
+#include "stdio.h"
 
 /**
- * main - entry point
- *
- * Return: 0 after printing the function
- */
-int main(void)
-{	
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void){
+	   puts("\"Programming is like building a multilingual puzzle");
+	      return (0);
 }
