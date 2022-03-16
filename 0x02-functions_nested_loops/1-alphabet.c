50bfab065d9adaa19ef2c446eb3978ec26dc7e05
mohammedmussa <99491339+mohammedmussa@users.noreply.github.com>
2022-03-16 23:04:18 +0300

    nl1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
-o
+#include "alx.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
